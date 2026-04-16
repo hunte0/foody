22883a4b75dea2e681b164e6489c04c7098869a5
Abidi-Rayen <killerab78@gmail.com>
2026-04-16 21:42:30 +0100

    front

---
+++ Front/next.config.ts
@@ -0,0 +1,8 @@
+// next.config.ts
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  devIndicators: false, // Removes the bottom-left icon
+}
+
+export default nextConfig
